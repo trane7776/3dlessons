93a0b14d90aa8072c316534970a5fd66d189700f
trane7776 <izbasarov.t@edu.rea.ru>
2025-06-21 23:57:53 +0300

    upd

--- 10-textures/src/script.js
+++ 10-textures/src/script.js
@@ -64,7 +64,8 @@ const scene = new THREE.Scene();
 
 /**
  * Object
- */
+
+*/
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 console.log(geometry.attributes);
 const material = new THREE.MeshBasicMaterial({ map: colorTexture });
