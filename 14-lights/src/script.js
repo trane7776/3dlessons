851691a06052998360be67e44c43716f421c02dd
trane7776 <izbasarov.t@edu.rea.ru>
2025-07-01 23:07:25 +0300

    upd

--- 14-lights/src/script.js
+++ 14-lights/src/script.js
@@ -106,9 +106,7 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-/**
- * Animate
- */
+
 const clock = new THREE.Clock()
 
 const tick = () =>
