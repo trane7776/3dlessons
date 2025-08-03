f356003071a8dc1166137e54dd34289c757fb331
trane7776 <izbasarov.t@edu.rea.ru>
2025-08-03 23:37:02 +0300

    particles

--- 17-particles/src/script.js
+++ 17-particles/src/script.js
@@ -18,15 +18,49 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('/textures/particles/2.png')
 
-/**
- * Test cube
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+
+
+
+// Particles
+// geometry
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 20000;
+
+const positions = new Float32Array(count * 3);
+const colors = new Float32Array(count * 3);
+
+
+for (let i = 0; i < count * 3; i++) {
+    positions[i] = (Math.random() - 0.5) * 10;
+    colors[i] = Math.random();
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+
+// material
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.1,
+    sizeAttenuation: true,
+})
+// particlesMaterial.color = new THREE.Color('#ff88cc')
+// particlesMaterial.map = particleTexture
+particlesMaterial.transparent = true
+particlesMaterial.alphaMap = particleTexture
+// particlesMaterial.alphaTest = 0.001
+// particlesMaterial.depthTest = false
+particlesMaterial.depthWrite = false;
+particlesMaterial.blending = THREE.AdditiveBlending
+particlesMaterial.vertexColors = true;
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
+
+
+
 
 /**
  * Sizes
@@ -80,6 +114,18 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // // Update particles
+    // particles.rotation.y = elapsedTime * 0.1
+
+    for (let i = 0; i < count; i++) {
+        const i3 = i * 3;
+        const x = particlesGeometry.attributes.position.array[i3];
+        const y = particlesGeometry.attributes.position.array[i3 + 1];
+        const z = particlesGeometry.attributes.position.array[i3 + 2];
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x);
+    }
+    particlesGeometry.attributes.position.needsUpdate = true;   
 
     // Update controls
     controls.update()
